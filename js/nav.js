15f2767ce7f8c035dde35818c26579c45a89056d
Kate Lilly <lillcat@umich.edu>
2024-11-09 17:28:19 -0500

    fix dynamic header to show active page

--- js/nav.js
+++ js/nav.js
@@ -2,12 +2,14 @@
 const navLinks = document.querySelectorAll('.nav-link');
 
 // Get the current page's URL
-const currentUrl = window.location.pathname.substring(1);
+const splitUrl = window.location.pathname.split("/");
+console.log(splitUrl)
+const currentPage = splitUrl[splitUrl.length - 1];
 
 // Iterate through each link and update the active class
 navLinks.forEach(link => {
   const page = link.getAttribute('href');
-  if (page === currentUrl) {
+  if (page === currentPage) {
     link.classList.add('active');
   } else {
     link.classList.remove('active');
